0089d6024b98b7c3f8278e97315518a088185355
Will Howes <williameldenhowes@gmail.com>
2019-09-19 15:41:22 +0100

    Method handles several letters with no duplicates

--- spec/duplicate_count_spec.rb
+++ spec/duplicate_count_spec.rb
@@ -1,6 +1,10 @@
 require 'duplicate_count'
 describe '#duplicate_count' do
-  it 'returns 0 for no duplicates' do
+  it 'returns 0 for a single letter' do
     expect(duplicate_count('a')).to eq(0)
+  end
+
+  it 'returns 0 for multiple letters with no duplicates' do
+    expect(duplicate_count('abcde')).to eq(0)
   end
 end
