64dad5cd497a63a2d75def276047af876927552c
Will Howes <williameldenhowes@gmail.com>
2019-09-19 15:56:36 +0100

    refactored to remove unnecessary variable

--- lib/duplicate_count.rb
+++ lib/duplicate_count.rb
@@ -1,9 +1,4 @@
 def duplicate_count(text_to_test)
-  unique_chars = text_to_test.split('').uniq
-  duplicate_chars = text_to_test.length - unique_chars.length
-  if duplicate_chars == nil
-    0
-  else
-    duplicate_chars
-  end
+  duplicate_chars = text_to_test.length - text_to_test.split('').uniq.length
+  duplicate_chars == nil ? 0 : duplicate_chars
 end
